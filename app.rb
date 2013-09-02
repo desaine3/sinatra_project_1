0a171207b3a9c0bcd97e95e8d735fdf51d98606e
Elina Desaine <edesaine@ticklo.com>
2013-09-02 17:32:14 +0100

    a

--- app.rb
+++ app.rb
@@ -3,3 +3,83 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/a' do
+    erb :a
+end
+get '/b' do
+    erb :b
+end
+get '/c' do
+    erb :c
+end
+get '/d' do
+    erb :d
+end
+get '/e' do
+    erb :e
+end
+get '/f' do
+    erb :f
+end
+get '/g' do
+    erb :g
+end
+get '/h' do
+    erb :h
+end
+get '/i' do
+    erb :i
+end
+get '/j' do
+    erb :j
+end
+get '/k' do
+    erb :k
+end
+get '/l' do
+    erb :l
+end
+get '/m' do
+    erb :m
+end
+get '/n' do
+    erb :n
+end
+get '/o' do
+    erb :o
+end
+get '/p' do
+    erb :p
+end
+get '/q' do
+    erb :q
+end
+get '/r' do
+    erb :r
+end
+get '/s' do
+    erb :s
+end
+get '/t' do
+    erb :t
+end
+get '/u' do
+    erb :u
+end
+get '/v' do
+    erb :v
+end
+get '/w' do
+    erb :w
+end
+get '/x' do
+    erb :x
+end
+get '/y' do
+    erb :y
+end
+get '/z' do
+    erb :z
+end
+
